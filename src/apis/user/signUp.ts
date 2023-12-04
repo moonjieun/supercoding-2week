caa4361aebb95268a72e2349b879e60da9de0cc2
moonjieun <sododls0330@naver.com>
2023-12-05 03:25:11 +0900

    refactor: signUp.ts api모듈을 사용한 요청으로 코드 수정

--- src/apis/user/signUp.ts
+++ src/apis/user/signUp.ts
@@ -1,22 +1,11 @@
-import axios from "axios";
+import api from "../interceptor";
 
 export const signUpUser = async (userData: unknown) => {
-  const response = await axios.post(
-    "https://pet-commerce.shop/v1/api/users",
-    userData
-  );
+  const response = await api.post("/v1/api/users", userData);
   return response.data;
 };
 
 export const checkEmail = async (email: string) => {
-  const response = await axios.post(
-    "https://pet-commerce.shop/v1/api/users/emailConfirm",
-    email,
-    {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
+  const response = await api.post("/v1/api/users/emailConfirm", email);
   return response;
 };
